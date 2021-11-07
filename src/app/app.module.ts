ef01843738a200f1b9f1a9c9af1656387cc29bd8
pedrosapiens <pedro@sapienslearning.com>
2021-11-07 13:40:33 +0100

    initial

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { InterpolationComponent } from './interpolation/interpolation.component';
+import { PropertyBindingComponent } from './property-binding/property-binding.component';
+import { EventBindingComponent } from './event-binding/event-binding.component';
+import { TwoWayBindingComponent } from './two-way-binding/two-way-binding.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InterpolationComponent,
+    PropertyBindingComponent,
+    EventBindingComponent,
+    TwoWayBindingComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/property-binding/property-binding.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-property-binding',
+  templateUrl: './property-binding.component.html',
+  styleUrls: ['./property-binding.component.css']
+})
+export class PropertyBindingComponent implements OnInit {
+
+  imagen: string = '/assets/img/santander-red.svg';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.cambiarImagen();
+  }
+
+  cambiarImagen(): void {
+    const logoRojo = '/assets/img/santander-red.svg';
+    const logoBlanco = '/assets/img/santander-white.svg';
+    setInterval(() => {
+      this.imagen === logoRojo ? this.imagen = logoBlanco : this.imagen = logoRojo;
+    }, 2000)
+  }
+
+}
